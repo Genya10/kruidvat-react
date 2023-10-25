d8edeaad476d805bd0a61135a0f5e537ac02f6d1
Jenya <113167825+Genya10@users.noreply.github.com>
2023-10-25 19:02:25 +0300

    add react-router-dom and few styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,18 @@ import cl from "../styles/Sidebar.module.css";
 export const Sidebar=()=>{
     return(
         <div className={cl.sidebar}>
-           Sidebar
+           <ul className={cl.contacts}>
+            <li className={cl.linkInfo}><img className={cl.social} src={"./img/gmail.png"}/>
+            <a href="mailto:kuzyashevqeka@gmail.com" className={cl.infoLi}>
+                kuzyashevqeka@gmail.com</a></li>
+            <li className={cl.linkInfo}><img className={cl.social} src={"./img/phone.png"}/>
+            <a href="tel:+380661542568" className={cl.infoLi}>
+                +38 066-154-25-68</a></li>
+            <li className={cl.linkInfo}><img className={cl.social} src={"./img/instagram.png"}/>
+            <a href="https://www.instagram.com/kruidvat.vitamins.ua/?igshid=ZjA0NjI3M2I%3D" target="_blank" 
+                className={cl.infoLi}>Instagram</a></li>
+           </ul>
+           <h2 className={cl.hit}>Хит продаж</h2>
         </div>
     )
 }
