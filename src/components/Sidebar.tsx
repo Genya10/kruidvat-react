83e339c82c11bc9debe53ebe9812561355a38cb4
Jenya <113167825+Genya10@users.noreply.github.com>
2023-10-26 11:42:09 +0300

    add ModalSidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import cl from "../styles/Sidebar.module.css";
+import { ModalSidebar } from "./ModalSidebar";
 
 export const Sidebar=()=>{
+
+    let [popup,setPopup]=useState(false);
+    const showPopup=()=>{
+        setPopup(popup = !popup);
+    }
 
     return(
         
         <div className={cl.sidebar}>
+
            <ul className={cl.contacts}>
             <li className={cl.linkInfo}><img className={cl.social} src={"./img/gmail.png"}/>
             <a href="mailto:kuzyashevqeka@gmail.com" className={cl.infoLi}>
@@ -20,33 +27,13 @@ export const Sidebar=()=>{
            <h2 className={cl.hit}>Хит продаж</h2>
            <div className={cl.hitPhoto}>
 
-     <img src={"./img-vitamins/omega3_60t.jpg"} alt="Omega3" className={cl.imgPhoto}/>
+     <img src={"./img-vitamins/omega3_60t.jpg"} alt="Omega3" 
+           onClick={showPopup} className={cl.imgPhoto}/>
  
   </div>
-    <div className={cl.popup}>
-    <div className={cl.popupClose}>Описание<span className={cl.popupCloseX}>X</span></div>
-  <div className={cl.popupText}>
-     <p>
-          Таблетки Омега-3 содержат длинноцепочечные ненасыщенные жирные кислоты, которые организм не производит сам, но которые
-          необходимо включать в рацион.<br/>
-          
-          📌Омега-3 жирные кислоты EPA и DHA способствуют нормальному функционированию сердца, а DHA способствует поддержанию
-          нормальной функции мозга.<br/>
-          
-          ❌Не подходит для детей младше 8 лет.<br/>
-          
-          ☝️Рекомендации по дозировке:
-          1 раз в день по 2 капсулы, запивая водой во время еды.
-          
-          Цена за 120 шт. 480 грн.
-          
-          Состав:<br/>
-          💊Рыбий жир 2000 мг;<br/>
-          💊Омега-3 жирные кислоты EPA 360 мг;<br/>
-          💊Омега-3 жирные кислоты DHA 240 мг;
-     </p>
-  </div>
-    </div>
+  {popup && 
+  <ModalSidebar showPopup={showPopup}/>} 
+    
         </div>
     )
 }
