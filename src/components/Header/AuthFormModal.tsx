187ab26a98d03cec17056a47f061bc47dc3df9be
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-01 15:46:43 +0200

    add empty value for input in AuthModal

--- src/components/Header/AuthFormModal.tsx
+++ src/components/Header/AuthFormModal.tsx
@@ -1,12 +1,11 @@
-import { useState,useEffect } from "react";
 import cl from "../../styles/AuthFormModal.module.css";
-
 
 type TypePropsAuthModal={
     closeModal:()=>void,
 }
 
 export const AuthFormModal=(props:TypePropsAuthModal)=>{
+
     const preventClose=(e:any)=>{
         e.stopPropagation();
     }
@@ -14,8 +13,8 @@ export const AuthFormModal=(props:TypePropsAuthModal)=>{
     return(
         <div className={cl.myModal} onClick={props.closeModal}>
             <form className={cl.myModalContent} onClick={preventClose}>
-           <input type="text" placeholder="enter gmail"/>
-           <input type="password" placeholder="enter password"/>
+           <input value="" type="text" placeholder="enter gmail"/>       
+           <input value="" type="password" placeholder="enter password"/>
            <button className={cl.btn}>Отправить</button>
             </form>
         </div>
